fffc293feabbac092ec7a3bb392dad4838984c13
deepivin <marek12306@gmail.com>
2020-10-18 16:49:41 +0200

    status and activity types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,4 @@
+import { EmojiType } from "./emoji.ts";
 import { GuildMemberType } from "./guild.ts";
 import { Snowflake } from "./utils.ts";
 
@@ -21,9 +22,50 @@ export interface UserMemberType extends UserType {
   member: GuildMemberType;
 }
 
+export interface ActivityTimestampsType {
+  start?: number;
+  end?: number;
+}
+
+export interface ActivityEmojiType {
+  name: string;
+  id?: Snowflake;
+  animated?: boolean;
+}
+
+export interface ActivityPartyType {
+  id?: string;
+  size?: number[];
+}
+
+export interface ActivityAssetsType {
+  large_image?: string;
+  large_text?: string;
+  small_image?: string;
+  small_text?: string;
+}
+
+export interface ActivitySecretsType {
+  join?: string;
+  spectate?: string;
+  match?: string;
+}
+
 export interface ActivityType {
   name: string;
   type: number;
+  url?: string;
+  created_at?: number;
+  timestamps?: ActivityTimestampsType[];
+  application_id?: Snowflake;
+  details?: string;
+  state?: string;
+  emoji?: ActivityEmojiType;
+  party?: ActivityPartyType;
+  assets?: ActivityAssetsType;
+  secrets?: ActivitySecretsType;
+  instance?: boolean;
+  flags?: number;
 }
 
 export interface StatusType {
