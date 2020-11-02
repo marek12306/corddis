09578047329f808424fdb8ae58234276c1c83ae8
deepivin <marek12306@gmail.com>
2020-11-02 16:02:13 +0100

    module fix

--- mod.ts
+++ mod.ts
@@ -1,16 +1,11 @@
-import { Client } from "./src/client/client.ts";
-import { EntityType } from "./src/types/utils.ts"
-import Intents from "./src/intents.ts"
-import { Message } from "./src/structures/message.ts"
-import { User } from "./src/structures/user.ts";
-import { Channel } from "./src/structures/channel.ts"
-import { Emoji } from "./src/structures/emoji.ts"
-import { Guild } from "./src/structures/guild.ts"
-import { GuildMember } from "./src/structures/guildMember.ts"
-import { Invite } from "./src/structures/invite.ts"
-import EmbedBuilder from "./src/embed.ts"
-
-export { 
-    Client, EntityType, Intents, Message, User, EmbedBuilder,
-    Channel, Emoji, Guild, GuildMember, Invite
-};
+export * from "./src/client/client.ts";
+export * from "./src/types/utils.ts"
+export { default as Intents } from "./src/intents.ts"
+export * from "./src/structures/message.ts"
+export * from "./src/structures/user.ts";
+export * from "./src/structures/channel.ts"
+export * from "./src/structures/emoji.ts"
+export * from "./src/structures/guild.ts"
+export * from "./src/structures/guildMember.ts"
+export * from "./src/structures/invite.ts"
+export { default as EmbedBuilder } from "./src/embed.ts"
