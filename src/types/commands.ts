a6a5b375095ce020d2fb6cabc68374856b1db262
deepivin <marek12306@gmail.com>
2020-12-12 13:07:32 +0100

    slash commands - types

---
+++ src/types/commands.ts
@@ -0,0 +1,87 @@
+import { EmbedType } from "./embed.ts";
+import { GuildMemberType } from "./guild.ts";
+import { AllowedMentionsType } from "./message.ts";
+import { Snowflake } from "./utils.ts";
+
+export interface ApplicationCommandsRootType {
+    id?: Snowflake;
+    application_id?: Snowflake;
+    name: string;
+    description: string;
+    options: ApplicationCommandOptionType[];
+}
+
+export interface ApplicationCommandOptionType {
+    name: string;
+    description: string;
+    type: number;
+    default?: boolean;
+    required?: boolean;
+    options?: ApplicationCommandOptionType[];
+    choices?: ApplicationCommandOptionChoiceType[];
+}
+
+export interface ApplicationCommandOptionChoiceType {
+    name: string;
+    value: string|number;
+}
+
+export enum ApplicationCommandOptionEnum {
+    SUB_COMMAND = 1,
+    SUB_COMMAND_GROUP,
+    STRING,
+    INTEGER,
+    BOOLEAN,
+    USER,
+    CHANNEL,
+    ROLE,
+}
+
+export interface InteractionType {
+    id: Snowflake;
+    type: InteractionEnum;
+    data?: ApplicationCommandInteractionDataType;
+    guild_id: Snowflake;
+    channel_id: Snowflake;
+    member: GuildMemberType;
+    token: string;
+    version: number;
+}
+
+export enum InteractionEnum {
+    Ping = 1,
+    ApplicationCommand
+}
+
+export interface ApplicationCommandInteractionDataType {
+    id: Snowflake;
+    name: string;
+    options: ApplicationCommandInteractionDataOptionType[];
+}
+
+export interface ApplicationCommandInteractionDataOptionType {
+    name: string;
+    value?: ApplicationCommandOptionType;
+    options?: ApplicationCommandInteractionDataOptionType[];
+}
+
+export interface InteractionResponseType {
+    type: InteractionResponseEnum;
+    data?: InteractionApplicationCommandCallbackDataType;
+}
+
+export enum InteractionResponseEnum {
+    Pong = 1,
+    Acknowledge,
+    ChannelMessage,
+    ChannelMessageWithSource,
+    ACKWithSource
+}
+
+export interface InteractionApplicationCommandCallbackDataType {
+    tts?: boolean;
+    content: string;
+    embeds?: EmbedType;
+    allowed_mentions?: AllowedMentionsType;
+    flags: number;
+}
